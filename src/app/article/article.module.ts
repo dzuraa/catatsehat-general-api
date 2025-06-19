eaab45e943118deee4565a98c2614eb4f7fec961
Ade Rifky Setiawan <100577989+adrfstwn@users.noreply.github.com>
2025-06-19 21:52:04 +0700

    Update article.module.ts

--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -3,8 +3,10 @@ import { ArticleHttpController } from './controllers';
 import { ArticleService } from './services';
 import { ArticleRepository } from './repositories';
 import { ArticleAdminHttpController } from './controllers/http/article-admin.controller';
+import { FileModule } from '../file'; 
 
 @Module({
+  imports: [FileModule],
   controllers: [ArticleHttpController, ArticleAdminHttpController],
   providers: [ArticleService, ArticleRepository],
   exports: [ArticleService, ArticleRepository],
