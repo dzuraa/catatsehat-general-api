2fd46557ff9839aaea3db97c9091edccb801a728
dzura <daffadoet17@gmail.com>
2025-06-29 14:43:02 +0700

    feat(blood-record): implement blood record public api

--- src/app/mother/blood-record/controllers/http/blood-record.controller.ts
+++ src/app/mother/blood-record/controllers/http/blood-record.controller.ts
@@ -23,6 +23,7 @@ import {
   CreateBloodRecordDto,
   UpdateBloodRecordDto,
 } from '../../dtos';
+import { BloodRecordPublicService } from '../../services/blood-record-public.service';
 
 @ApiTags('[ADMIN] Blood Record')
 @ApiSecurity('JWT')
@@ -118,6 +119,33 @@ export class BloodRecordAdminHttpController {
         message: 'Data deleted successfully',
       });
     } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
+  }
+}
+
+@ApiTags('[PUBLIC] Blood Record')
+@Controller({
+  path: 'public/bloodRecord',
+  version: '1',
+})
+export class BloodRecordPublicHttpController {
+  constructor(
+    private readonly bloodRecordPublicService: BloodRecordPublicService,
+  ) {}
+
+  @Post()
+  public async create(@Body() createBloodRecordDto: CreateBloodRecordDto) {
+    try {
+      const data =
+        await this.bloodRecordPublicService.create(createBloodRecordDto);
+      return new ResponseEntity({
+        data,
+        status: HttpStatus.CREATED,
+        message: 'Data created successfully',
+      });
+    } catch (error) {
+      console.log(error);
       throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
     }
   }
