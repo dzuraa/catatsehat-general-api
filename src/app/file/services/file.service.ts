f8a733db7fcb5503a6cee338228c92f399bf17d3
dzura <daffadoet17@gmail.com>
2025-07-08 09:53:55 +0700

    fix(minio): error mixed content from non secure backend server

--- src/app/file/services/file.service.ts
+++ src/app/file/services/file.service.ts
@@ -89,7 +89,11 @@ export class FileService {
       fileName: payload.fileName,
       mimeType: payload.mimeType,
       type,
-      path: [ENV.S3_ENDPOINT, ENV.S3_BUCKET_NAME, payload.fileName].join('/'),
+      path: [
+        'https://catatsehat.site/minio',
+        ENV.S3_BUCKET_NAME,
+        payload.fileName,
+      ].join('/'),
       size: file.length,
     });
   }
